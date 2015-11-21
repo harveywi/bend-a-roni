8a1210167e1968f7f1d33947cec20c711d1cf974
William Harvey <harveywi@cse.ohio-state.edu>
2015-11-21 14:28:49 -0500

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,10 @@
+Bend-A-Roni
+=====================
+
+Bend-A-Roni is a [Scala](http://www.scala-lang.org) implementation of
+"As-Rigid-As-Possible Shape Manipulation" (Takeo Igarashi, Tomer Moscovich,
+John F. Hughes, "As-Rigid-As-Possible Shape Manipulation", ACM Transactions on
+Computer Graphics, Vol.24, No.3, ACM SIGGRAPH 2005, Los Angels, USA,
+2005. [PDF](http://www-ui.is.s.u-tokyo.ac.jp/~takeo/papers/rigid.pdf)).
+
+Stay tuned for more.
---
+++ project/Build.scala
@@ -0,0 +1,53 @@
+import sbt._
+import Keys._
+
+object BendARoniBuild extends Build {
+
+  lazy val bendARoni = (project in file(".")
+    aggregate(core, examples)
+    dependsOn(core, examples)
+    settings(commonSettings: _*)
+    settings(
+      moduleName := "bend-a-roni-root"
+    )
+  )
+
+  lazy val core = (project
+    settings(commonSettings: _*)
+    settings(
+      moduleName := "bend-a-roni"
+    )
+  )
+
+
+  lazy val examples = (project
+    dependsOn core
+    settings(commonSettings: _*)
+    settings(
+      moduleName := "bend-a-roni-examples"
+    )
+  )
+
+  def commonSettings = 
+    Seq(
+      organization := "org.aylasoftware",
+      scalaVersion := "2.11.7",
+      scalacOptions := Seq(
+          "-feature",
+          "-language:higherKinds",
+          "-Xfatal-warnings",
+          "-deprecation",
+          "-unchecked"
+      ),
+
+      resolvers ++= Seq(
+        Resolver.sonatypeRepo("releases"),
+        Resolver.sonatypeRepo("snapshots")
+      ),
+ 
+      libraryDependencies ++= Seq(
+        "com.chuusai" %% "shapeless" % "2.2.5" withSources(),
+        "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
+      )
+    )
+}
